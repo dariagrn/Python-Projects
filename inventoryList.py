ac4b68ec78076953ade91080d7255c41131772bd
Daria Green <daria.green20@gmail.com>
2025-08-06 18:30:47 -0600

    Add files via upload

--- inventoryList.py
+++ inventoryList.py
@@ -74,11 +74,16 @@ price =[
 
 print("\nWelcome to Daria's Market Inventory List")
 print("Here are the items I have available for sale:")
+
+#item listing printed. with the statement below
 for i in range(len(itemName)):
     print(f"\n{itemName[i]}")
+
+#ask user to select an item to display details
 print("\nPlease enter the name of the item you would like to purchase:")
 item = input().strip()
 
+#if/else statement to display the item details the user chose or presented with Item not found
 if item in itemName:
     index = itemName.index(item)
     print(f"\nYou have selected {item}.")
@@ -86,3 +91,10 @@ if item in itemName:
     print(f"\nAvailable Inventory: {inventoryAmount[index]}")
 else:
     print("\nItem not found.")
+
+print("\nWould you like to order this item? (yes/no)")
+order = input().strip().lower()
+if order == "yes":
+    print(f"\nThank you for your order of {item}.")
+else:
+    print("\nOrder canceled.")
