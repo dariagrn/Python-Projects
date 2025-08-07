d0be88047e73ab53d68c56488cabdd41242fc8e9
Daria Green <daria.green20@gmail.com>
2025-08-06 18:25:05 -0600

    Add files via upload

---
+++ inventoryList.py
@@ -0,0 +1,88 @@
+#Programmer: Daria Green
+#Email: Daria.green20@gmail.com
+#Purpose: Market Items Inventory List
+
+#Items I offer
+itemName =[
+    'Mystery Pouch',
+    'Bead KeyChain',
+    'Book Sleeve',
+    'Bandana',
+    'Amigurumi Sticks',
+    'Opctopus Plushy',
+    'Mushroom Fidget Toy',
+    'Rosette Purse (Small)',
+    'Rosette Purse (Large)',
+    'Granny Square XL Purse',
+    'Peach Granny Square Purse',
+    'Sparkle Blue Totebag',
+    'Bow Clip',
+    'Spa Set',
+    'Pumpkin Plushy',
+    'Flower Coaster Set',
+    'Pencil Pouch',
+    'Mushroom Pillow',
+    'Granny Square Runner',
+    'Crochet Blanket'
+]
+
+inventoryAmount =[
+    '30',
+    '12',
+    '10',
+    '10',
+    '15',
+    '7',
+    '15',
+    '5',
+    '5',
+    '1',
+    '1',
+    '1',
+    '15',
+    '10',
+    '10',
+    '5',
+    '5',
+    '5',
+    '2',
+    '2'
+]
+
+price =[
+    '10',
+    '12',
+    '20',
+    '10',
+    '15',
+    '10',
+    '10',
+    '45',
+    '55',
+    '90',
+    '40',
+    '55',
+    '10',
+    '35',
+    '25',
+    '25',
+    '12',
+    '70',
+    '100',
+    '300'
+]
+
+print("\nWelcome to Daria's Market Inventory List")
+print("Here are the items I have available for sale:")
+for i in range(len(itemName)):
+    print(f"\n{itemName[i]}")
+print("\nPlease enter the name of the item you would like to purchase:")
+item = input().strip()
+
+if item in itemName:
+    index = itemName.index(item)
+    print(f"\nYou have selected {item}.")
+    print(f"\nPrice: ${price[index]}")
+    print(f"\nAvailable Inventory: {inventoryAmount[index]}")
+else:
+    print("\nItem not found.")
